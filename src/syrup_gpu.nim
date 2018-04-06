2096eb91ba769ffd7706f8917a20e1a419dde05c
emekoi <emekankurumeh@outlook.com>
2018-04-06 13:32:50 -0500

    SDL_gpu maybe

--- src/syrup_gpu.nim
+++ src/syrup_gpu.nim
@@ -5,19 +5,11 @@
 ##  under the terms of the MIT license. See LICENSE for details.
 ##
 
-when defined(SYRUP_GL):
-  when not defined(MODE_RGBA):
-    {.fatal: "compile syrup with the flag MODE_RGBA".}
-  import
-    sdl2/sdl, suffer, os, tables,
-    syrup/[timer, embed, shader, gl, mixer],
-    syrup/[system, input, gifwriter]
-else:
-  import
-    sdl2/sdl, suffer, os, tables,
-    syrup/[timer, embed, shader, mixer],
-    syrup/[system, input, gifwriter]
-  
+import
+  sdl2/sdl, suffer, os, tables, sdl2/sdl_gpu as gpu,
+  syrup/[timer, embed, shader, mixer],
+  syrup/[system, input, gifwriter]
+
 export
   suffer,
   timer,
@@ -26,29 +18,14 @@ export
   system
   # gifwriter
 
-when defined(SYRUP_GL):
-  type
-    GLHandle = ref object
-      context*: sdl.GLContext
-      vbo*: gl.BufferId
-      vao*: gl.VertexArrayId
-      ebo*: gl.BufferId
-      tex*: gl.TextureId
-      shader*: Shader
 
-    Context = ref object
-      running*: bool
-      window*: sdl.Window
-      context: sdl.GLContext
-      handle: GLHandle
-      canvas*: Buffer
-else:
-  type Context = ref object
-    running*: bool
-    window*: sdl.Window
-    surface*: sdl.Surface
-    canvas*: Buffer
-    canvas_size: int
+type Context* = ref object
+  running*: bool
+  window*: sdl.Window
+  target*: gpu.Target
+  screen*: gpu.Image
+  canvas*: Buffer
+  bpr: int
 
 type Config* = tuple
   title: string
@@ -58,20 +35,6 @@ type Config* = tuple
   bordered: bool
   clear: Pixel
   fps: float
-  
-
-when defined(SYRUP_GL):
-  let
-    VERTICIES = [
-      #  Position                Texcoords
-      -1.0f,  1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 1.0f, 1.0f, # Top-left
-       1.0f,  1.0f, 1.0f, 1.0f,   1.0f, 0.0f, 1.0f, 1.0f, # Top-right
-       1.0f, -1.0f, 1.0f, 1.0f,   1.0f, 1.0f, 1.0f, 1.0f, # Bottom-right
-      -1.0f, -1.0f, 1.0f, 1.0f,   0.0f, 1.0f, 1.0f, 1.0f, # Bottom-left
-    ]
-    ELEMENTS = [
-      0, 1, 2, 3,
-    ]
 
 let DEFAULT_FONT = newFontString(DEFAULT_FONT_DATA, DEFAULT_FONT_SIZE)
 
@@ -85,83 +48,38 @@ var
   
 proc finalize(ctx: Context) =
   mixer.deinit()
-  ctx.window.destroyWindow()
-  when defined(SYRUP_GL):
-    ctx.context.glDeleteContext()
-  sdl.quit()
-  
-when defined(SYRUP_GL):
-  proc finalize(handle: GLHandle) =
-    gl.deleteBuffers([handle.ebo, handle.vbo])
-    gl.deleteVertexArray(handle.vao)
-    gl.deleteTexture(handle.tex)
+  # ctx.window.destroyWindow()
+  ctx.screen.freeImage()
+  gpu.quit()
+
 
 proc setup() =
   new(CORE, finalize)
-  when defined(SYRUP_GL):
-    new(CORE.handle, finalize)
 
-  var flags = 0'u32
-
+  # init SDL and SDL_gpu
+  gpu.setDebugLevel(gpu.DEBUG_LEVEL_MAX)
   if sdl.init(sdl.INIT_VIDEO) != 0:
     quit "ERROR: can't initialize SDL video: " & $sdl.getError()
 
-  when defined(SYRUP_GL):
-    if sdl.glSetAttribute(sdl.GL_CONTEXT_MAJOR_VERSION, 2) != 0:
-      quit "ERROR: unable set GL_CONTEXT_MAJOR_VERSION attribute: " & $sdl.getError()
-
-    if sdl.glSetAttribute(sdl.GL_CONTEXT_MINOR_VERSION, 1) != 0:
-      quit "ERROR: unable set GL_CONTEXT_MINOR_VERSION attribute: " & $sdl.getError()
-
-    if sdl.glSetAttribute(sdl.GL_DOUBLEBUFFER, GL_TRUE.cint) != 0:
-      quit "ERROR: unable set GL_DOUBLEBUFFER attribute: " & $sdl.getError()
-    
-    if sdl.glSetAttribute(sdl.GL_ACCELERATED_VISUAL, 1) != 0:
-      quit "ERROR: unable set GL_ACCELERATED_VISUAL attribute: " & $sdl.getError()
-    
-    flags = sdl.WINDOW_OPENGL
-
-  # Create window
-  CORE.window = sdl.createWindow(
-    SETTINGS.title,
-    sdl.WindowPosUndefined,
-    sdl.WindowPosUndefined,
-    SETTINGS.width, SETTINGS.height,
-    flags)
-
-  if CORE.window == nil:
-    quit "ERROR: can't create window: " & $sdl.getError()
-
-  when defined(SYRUP_GL):
-    CORE.context = sdl.glCreateContext(CORE.window)
-
-    if CORE.context == nil:
-      quit "ERROR: can't create OpenGL context: " & $sdl.getError()
-
-    loadExtensions()
-
-    gl.disable(CULL_FACE)
-    gl.disable(DEPTH_TEST)
-
-    CORE.handle.vao = gl.genBindVertexArray()
-    
-    CORE.handle.vbo = gl.genBindBufferData(BufferTarget.ARRAY_BUFFER, VERTICIES, BufferDataUsage.STATIC_DRAW)
-    CORE.handle.ebo = gl.genBindBufferData(BufferTarget.ELEMENT_ARRAY_BUFFER, ELEMENTS, BufferDataUsage.STATIC_DRAW);
-    
-    CORE.handle.shader = newShaderFromMem(DEFAULT_FRAG_DATA)
-    CORE.handle.shader.use()
-
-    CORE.handle.tex = gl.genBindTexture(TextureTarget.TEXTURE_2D)
-    gl.texParameteri(TextureTarget.TEXTURE_2D, TextureParameter.TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER)
-    gl.texParameteri(TextureTarget.TEXTURE_2D, TextureParameter.TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER)
-    gl.texParameteri(TextureTarget.TEXTURE_2D, TextureParameter.TEXTURE_BASE_LEVEL, 0)
-    gl.texParameteri(TextureTarget.TEXTURE_2D, TextureParameter.TEXTURE_MAX_LEVEL, 0)
-    gl.texParameteri(TextureTarget.TEXTURE_2D, TextureParameter.TEXTURE_MIN_FILTER, GL_NEAREST)
-    gl.texParameteri(TextureTarget.TEXTURE_2D, TextureParameter.TEXTURE_MAG_FILTER, GL_NEAREST)
+  when defined(USE_GL2) or true:
+    CORE.target = gpu.initRenderer(gpu.RENDERER_OPENGL_2,
+    SETTINGS.width.uint16, SETTINGS.height.uint16, gpu.INIT_DISABLE_VSYNC)
   else:
-    CORE.surface = CORE.window.getWindowSurface()
-    CORE.canvas_size = SETTINGS.width * SETTINGS.height * sizeof(Pixel)
+    CORE.target = gpu.init(SETTINGS.width.uint16, SETTINGS.height.uint16, gpu.INIT_DISABLE_VSYNC)
 
+  CORE.screen = gpu.createImage(SETTINGS.width.uint16, SETTINGS.height.uint16, gpu.FORMAT_RGBA)
+  CORE.screen.setRGB(255, 255, 0)
+  let renderer = gpu.getCurrentRenderer()
+  let id = renderer.id
+  
+  gpu.logInfo("\nusing renderer: %s (%d.%d)\n", id.name, id.major_version, id.minor_version)
+  gpu.logInfo(" shader versions supported: %d to %d\n\n", renderer.min_shader_version, renderer.max_shader_version)
+
+  CORE.window = sdl.getWindowFromID(gpu.getInitWindow())
+  if CORE.window == nil:
+    echo "ERROR: can't find SDL window: " & $sdl.getError()
+  
+  CORE.bpr = SETTINGS.width * sizeof(Pixel)
   CORE.canvas = newBuffer(SETTINGS.width, SETTINGS.height)
 
   mixer.init()
@@ -185,10 +103,8 @@ proc run*(update: proc(dt: float), draw: proc()) =
       updateFunc(timer.getDelta())
 
     # clear the screen
-    when defined(SYRUP_GL):
-      gl.clearColor(0.0f, 0.0f, 0.0f, 1.0f);    
-      gl.clear(BufferMask.DEPTH_BUFFER_BIT, BufferMask.COLOR_BUFFER_BIT);
     CORE.canvas.clear(SETTINGS.clear)
+    CORE.target.clear()
 
     # run the draw callback
     if drawFunc != nil:
@@ -197,20 +113,10 @@ proc run*(update: proc(dt: float), draw: proc()) =
     input.reset()
 
     # draw the buffer to the screen
-    when defined(SYRUP_GL):
-      let buf = CORE.canvas
-      gl.texImage2D(TexImageTarget.TEXTURE_2D, 0, TextureInternalFormat.RGBA, buf.w, buf.h,
-        PixelDataFormat.RGBA, PixelDataType.UNSIGNED_BYTE, buf.pixels)
-      gl.drawElements(gl.DrawMode.QUADS, 4, IndexType.UNSIGNED_INT, 0)
-      sdl.glSwapWindow(CORE.window)
-    else:
-      if CORE.surface != nil and CORE.surface.mustLock():
-        if CORE.surface.lockSurface() != 0:
-          quit "ERROR: couldn't lock screen: " & $sdl.getError()
-      copyMem(CORE.surface.pixels, CORE.canvas.pixels[0].addr, CORE.canvas_size)
-      if CORE.surface.mustLock(): CORE.surface.unlockSurface()
-      if CORE.window.updateWindowSurface() != 0:
-        quit "ERROR: couldn't update screen: " & $sdl.getError()
+    CORE.screen.updateImageBytes(nil,
+    cast[ptr cuchar](addr CORE.canvas.pixels[0]), CORE.bpr)
+    gpu.blit(CORE.screen, nil, CORE.target, 0, 0)
+    CORE.target.flip()
 
     # wait for next frame
     let step = 1.0 / SETTINGS.fps
@@ -222,20 +128,20 @@ proc run*(update: proc(dt: float), draw: proc()) =
     else:
       last = now
 
+
 proc exit*() =
   CORE.running = false
 
 # CONFIG
 proc resetVideoMode() =
-  discard sdl.setWindowFullscreen(CORE.window, if SETTINGS.fullscreen: sdl.WINDOW_FULLSCREEN_DESKTOP else: 0)
-  sdl.setWindowSize(CORE.window, SETTINGS.width, SETTINGS.height)
-  sdl.setWindowResizable(CORE.window, if SETTINGS.resizable: true else: false)
-  sdl.setWindowBordered(CORE.window, if SETTINGS.bordered: true else: false)
+  CORE.bpr = SETTINGS.width * sizeof(Pixel)  
+  discard gpu.setFullscreen(SETTINGS.fullscreen, true)
+  discard gpu.setWindowResolution(SETTINGS.width.uint16, SETTINGS.height.uint16)
+  # sdl.setWindowResizable(CORE.window, if SETTINGS.resizable: true else: false)
+  # sdl.setWindowBordered(CORE.window, if SETTINGS.bordered: true else: false)
   CORE.canvas.resize(SETTINGS.width, SETTINGS.height)
   CORE.canvas.reset()
-  when not defined(SYRUP_GL):
-    CORE.canvas_size = SETTINGS.width * SETTINGS.height * sizeof(Pixel)
-    
+
   
 proc getConfig*(): Config =
   SETTINGS
@@ -374,8 +280,8 @@ proc displace*(src, map: Buffer, channelX, channelY: char, scaleX, scaleY: int) 
 proc blur*(src: Buffer, radiusx, radiusy: int) = CORE.canvas.blur(src, radiusx, radiusy)
 
 # FONT
-proc fontFromDefault*(ptsize: float=DEFAULT_FONT_SIZE): Font =
-  newFontString(DEFAULT_FONT_DATA, ptsize)
+# proc fontFromDefault*(ptsize: float=DEFAULT_FONT_SIZE): Font =
+#   newFontString(DEFAULT_FONT_DATA, ptsize)
 
 if CORE == nil:
   setup()
